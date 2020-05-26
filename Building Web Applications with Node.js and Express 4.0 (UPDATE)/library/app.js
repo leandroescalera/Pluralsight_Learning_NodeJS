dc8de7626a94cd03ca936e6e4b0687649b8d1c4e
Leandro Escalera <marcelo.escalera.inf@hotmail.com>
2020-05-26 00:25:06 -0400

    Commit 18 20200525
    Se encuentra a la mitad del modulo 8 authentication

--- Building Web Applications with Node.js and Express 4.0 (UPDATE)/library/app.js
+++ Building Web Applications with Node.js and Express 4.0 (UPDATE)/library/app.js
@@ -8,16 +8,20 @@ let morgan = require("morgan");
 let path = require("path");
 const configGeneral = require("./config");
 const sql = require("mssql");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
 
 const config = {
-  user: "library",
-  password: "8353086ASlom",
-  server: "pslibrary8353086.database.windows.net", // You can use 'localhost\\instance' to connect to named instance
-  database: "PSLibrary",
-  options: {
-    encrypt: true,
-    enableArithAbort: true,
-  },
+    user: "library",
+    password: "8353086ASlom",
+    server: "pslibrary8353086.database.windows.net", // You can use 'localhost\\instance' to connect to named instance
+    database: "PSLibrary",
+    options: {
+        encrypt: true,
+        enableArithAbort: true,
+    },
 };
 
 let app = express();
@@ -25,69 +29,78 @@ let app = express();
 sql.connect(config).catch((err) => debug(err));
 
 app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(
+    bodyParser.urlencoded({
+        extended: false,
+    })
+);
+app.use(cookieParser());
+app.use(session({ secret: "library" }));
+require("./src/config/passport.js")(app);
 
 app.use((req, res, next) => {
-  debug("My Middleware");
-  next();
+    debug("My Middleware");
+    next();
 });
 
 app.use(express.static(path.join(__dirname, "/public/")));
 
 app.use(
-  "/css",
-  express.static(path.join(__dirname, "/node_modules/bootstrap/dist/css"))
+    "/css",
+    express.static(path.join(__dirname, "/node_modules/bootstrap/dist/css"))
 );
 app.use(
-  "/js",
-  express.static(path.join(__dirname, "/node_modules/bootstrap/dist/js"))
+    "/js",
+    express.static(path.join(__dirname, "/node_modules/bootstrap/dist/js"))
 );
 app.use(
-  "/js",
-  express.static(path.join(__dirname, "/node_modules/jquery/dist"))
+    "/js",
+    express.static(path.join(__dirname, "/node_modules/jquery/dist"))
 );
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
-const nav = [
-  {
-    link: "/books",
-    title: "Book",
-  },
-  {
-    link: "/authors",
-    title: "Author",
-  },
+const nav = [{
+        link: "/books",
+        title: "Book",
+    },
+    {
+        link: "/authors",
+        title: "Author",
+    },
 ];
 
 const bookRouter = require("./src/routes/bookRoutes")(nav);
 const adminRouter = require("./src/routes/adminRoutes")(nav);
+const authRouter = require("./src/routes/authRoutes")(nav);
 
 app.use("/books", bookRouter);
 app.use("/admin", adminRouter);
+app.use("/auth", authRouter);
 
 app.get("/", (req, res) => {
-  //res.send('Hello from my library app');
-  // res.sendFile(path.join(__dirname, '/views/', '/index.html'));
-  res.render("index", {
-    nav: [
-      {
-        link: "/books",
-        title: "Book",
-      },
-      {
-        link: "/authors",
-        title: "Author",
-      },
-    ],
-    title: "Library",
-  });
+    //res.send('Hello from my library app');
+    // res.sendFile(path.join(__dirname, '/views/', '/index.html'));
+    res.render("index", {
+        nav: [{
+                link: "/books",
+                title: "Book",
+            },
+            {
+                link: "/authors",
+                title: "Author",
+            },
+        ],
+        title: "Library",
+    });
 });
 
 app.listen(configGeneral.port, () => {
-  debug(
-    chalk.blue("Listenning on PORT") +
-      "  >>>>  " +
-      chalk.green(`http://localhost:${configGeneral.port}`)
-  );
-  //console.log(chalk.blue("Listenning on PORT 3000") + "  >>>>  " + chalk.green(`http://localhost:${3000}`));
+    debug(
+        chalk.blue("Listenning on PORT") +
+        "  >>>>  " +
+        chalk.green(`http://localhost:${configGeneral.port}`)
+    );
+    //console.log(chalk.blue("Listenning on PORT 3000") + "  >>>>  " + chalk.green(`http://localhost:${3000}`));
 });
