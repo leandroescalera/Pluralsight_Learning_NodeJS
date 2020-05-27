1030fa596f578ad7e9bec2b9aa49cbdae8df14c0
Leandro Escalera <marcelo.escalera.inf@hotmail.com>
2020-05-27 01:41:32 -0400

    Commit 19 20200526
    Se concluyo con el curso intermedio de Pluralsaight Building Web Applications with Node.js and Express 4.0 (UPDATE)

--- Building Web Applications with Node.js and Express 4.0 (UPDATE)/library/src/routes/bookRoutes.js
+++ Building Web Applications with Node.js and Express 4.0 (UPDATE)/library/src/routes/bookRoutes.js
@@ -1,12 +1,8 @@
 let express = require('express');
 const bookRouter = express.Router();
 const sql = require('mssql');
-const {
-    MongoClient,
-    ObjectID
-} = require('mongodb');
-const debug = require('debug')('app:bookRoutes');
-
+const bookController = require('../controllers/bookController');
+const bookService = require('../services/goodreadsService');
 
 function router(nav) {
 
@@ -25,30 +21,16 @@ function router(nav) {
     //         }());
 
     //     });
+    const {
+        getIndex,
+        getById,
+        middleware
+    } = bookController(bookService, nav);
+
+    bookRouter.use(middleware);
 
     bookRouter.route('/')
-        .get((req, res) => {
-            const url = 'mongodb://localhost:27017';
-            const dbName = 'libraryApp';
-            (async function mongo() {
-                let client;
-                try {
-                    client = await MongoClient.connect(url);
-                    debug('Connect correctly to server...');
-                    const db = client.db(dbName);
-                    const col = await db.collection('books')
-                    const books = await col.find().toArray();
-                    res.render('bookListView', {
-                        nav,
-                        title: 'Library',
-                        books
-                    });
-                } catch (error) {
-                    debug(error.stack);
-                }
-                client.close();
-            }());
-        });
+        .get(getIndex);
 
     // bookRouter.route('/:id')
     //     .all((req, res, next) => {
@@ -73,34 +55,7 @@ function router(nav) {
     //     })
 
     bookRouter.route('/:id')
-        .get((req, res) => {
-            const url = 'mongodb://localhost:27017';
-            const dbName = 'libraryApp';
-            const {
-                id
-            } = req.params;
-
-            (async function mongo() {
-                let client;
-                try {
-                    client = await MongoClient.connect(url);
-                    debug('Connect correctly to server...');
-                    const db = client.db(dbName);
-                    const col = await db.collection('books');
-                    const book = await col.findOne({
-                        _id: new ObjectID(id)
-                    });
-                    debug(book);
-                    res.render('bookView', {
-                        nav,
-                        title: 'Library',
-                        book
-                    });
-                } catch (err) {
-                    debug(err.stack);
-                }
-            }());
-        });
+        .get(getById);
     return bookRouter;
 }
 
